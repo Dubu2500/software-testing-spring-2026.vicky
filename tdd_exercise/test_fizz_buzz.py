29db548d4f970417d2717740475d7ca301fea756
Dubu2500 <145612999+Dubu2500@users.noreply.github.com>
2026-04-08 12:41:47 -0600

    Exercise 1 Test Requirement 3

--- tdd_exercise/test_fizz_buzz.py
+++ tdd_exercise/test_fizz_buzz.py
@@ -19,10 +19,22 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(fizz_buzz(1), "1")
         self.assertEqual(fizz_buzz(-1), "-1")
 
-    def test_from_number_multiple_three_return_fizz(self, ):
+    def test_from_number_multiple_three_return_fizz(
+        self,
+    ):
         """
         2. Metodo que recibe un numero multiplo de 3 de input y devuelve "Fizz"
         """
-        self.assertEqual(fizz_buzz.fizz_buzz(-3), "Fizz")
-        self.assertEqual(fizz_buzz.fizz_buzz(0), "Fizz")
-        self.assertEqual(fizz_buzz.fizz_buzz(3), "Fizz")
+        self.assertEqual(fizz_buzz(-3), "Fizz")
+        self.assertEqual(fizz_buzz(0), "Fizz")
+        self.assertEqual(fizz_buzz(3), "Fizz")
+
+    def test_from_number_multiple_three_return_buzz(
+        self,
+    ):
+        """
+        3. Metodo que recibe un numero multiplo de 5 de input y devuelve "Buzz"
+        """
+        self.assertEqual(fizz_buzz(-5), "Buzz")
+        self.assertEqual(fizz_buzz(0), "Buzz")
+        self.assertEqual(fizz_buzz(5), "Buzz")
