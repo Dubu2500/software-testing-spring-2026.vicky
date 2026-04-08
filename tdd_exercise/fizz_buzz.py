334f94256c4beed8554f3e3c90bf5994e3240fde
Dubu2500 <145612999+Dubu2500@users.noreply.github.com>
2026-04-08 12:37:48 -0600

    Exercise 1 Requirement 2

--- tdd_exercise/fizz_buzz.py
+++ tdd_exercise/fizz_buzz.py
@@ -3,10 +3,15 @@ Este modulo contiene la logica para las pruebas del ejercicio de FizzBuzz
 """
 
 
-def fizz_buzz(input):
+def fizz_buzz(number):
     """
     Metodo que recibe numeros y devuelve un string segun los requerimientos de FizzBuzz
     """
+    # 1. Numero a string
+    output = str(number)
 
-    """1. Numero a string"""
-    output = str(input)
+    # 2. Multiplos de 3
+    if number % 3 == 0:
+        output = "Fizz"
+
+    return output
