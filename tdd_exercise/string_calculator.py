6178de7ecaa7acca0749f167ef220976a56641f0
vela <dvela020@gmail.com>
2026-04-08 17:02:53 -0600

    Exercise 2 Requirement 5

--- tdd_exercise/string_calculator.py
+++ tdd_exercise/string_calculator.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 """
-Este modulo contiene la logica para las pruebas del ejercicio de String Calculator
+Este modulo contiene la logica para las pruebas del ejercicio de String Calculator 
 """
 
 import re
@@ -7,7 +9,8 @@ import re
 
 def add(message):
     """
-    Recibe un string con numeros y devuelve su suma.
+    Metodo que recibe un string con numeros,
+    procesa segun los requerimientos de String Calculator y devuelve su suma
     """
 
     # 1. String vacio a 0
@@ -15,22 +18,46 @@ def add(message):
         return 0
 
     errors = []
-    delimiter = ","
+    delimiter = ",|\n"
     numbers_part = message
+
+    # 5. Delimitador personalizado
+    if message.startswith("//"):
+        parts = message.split("\n", 1)
+        delimiter_part = parts[0][2:]
+        delimiter = re.escape(delimiter_part)
+        numbers_part = parts[1]
+
+        # Validar caracteres inesperados
+        i = 0
+        while i < len(numbers_part):
+            character = numbers_part[i]
+            if character.isdigit() or character == delimiter_part:
+                i += 1
+            else:
+                errors.append(
+                    f"expected '{delimiter_part}' but found '{character}' at position {i}"
+                )
+                break
+
+    # 4. Validar separador al final
+    if re.search(rf"{delimiter}$", numbers_part):
+        errors.append("Separator at end not allowed")
 
     # 2 y 3. Dividir usando el delimitador
     raw_numbers = re.split(delimiter, numbers_part)
 
     integers = []
+    negatives = []
 
     for item in raw_numbers:
         if item == "":
             continue
         if not item.lstrip("-").isdigit():
             errors.append(f"Invalid number: '{item}'")
-
-    # 4. Validar separador al final
-    if re.search(rf"{delimiter}$", numbers_part):
-        errors.append("Separator at end not allowed")
+            continue
+        num = int(item)
+        if num < 0:
+            negatives.append(num)
 
     return sum(integers)
