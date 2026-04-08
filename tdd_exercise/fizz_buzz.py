73468f1c1d67568e8dd7a96870368a10196a386f
vela <dvela020@gmail.com>
2026-04-08 12:32:03 -0600

    Exercise 1 Requirement 1

--- tdd_exercise/fizz_buzz.py
+++ tdd_exercise/fizz_buzz.py
@@ -3,7 +3,10 @@ Este modulo contiene la logica para las pruebas del ejercicio de FizzBuzz
 """
 
 
-def fizz_buzz():
+def fizz_buzz(input):
     """
     Metodo que recibe numeros y devuelve un string segun los requerimientos de FizzBuzz
     """
+
+    """1. Numero a string"""
+    output = str(input)
