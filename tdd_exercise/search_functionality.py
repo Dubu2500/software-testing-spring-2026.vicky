91d6f6c55a18167f738ed305f1856b24740cd967
vela <dvela020@gmail.com>
2026-04-13 12:17:16 -0600

    feat: Exercise 4 Test Requirement 1

--- tdd_exercise/search_functionality.py
+++ tdd_exercise/search_functionality.py
@@ -3,3 +3,28 @@
 """
 Este modulo contiene la logica para las pruebas del ejercicio de FizzBuzz
 """
+
+import json
+import os
+
+
+def search(str_to_search):
+    """
+    Metodo que recibe un string y devuelve el numero de ciudades encontradas
+    """
+
+    # Load cities from JSON file
+    cities_file_path = os.path.join(os.path.dirname(__file__), "cities.json")
+    with open(cities_file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        cities = data["cities"]
+
+    cities_found = []
+
+    city_search = str_to_search
+
+    for city in cities:
+        if city_search in city:
+            cities_found.append(city)
+
+    return cities_found
