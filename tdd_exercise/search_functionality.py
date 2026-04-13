28382bf09b79e6dbddc45b6bb35ee419ce8a52b1
vela <dvela020@gmail.com>
2026-04-13 11:43:27 -0600

    feat: documentos para ejercicio 4

---
+++ tdd_exercise/search_functionality.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""
+Este modulo contiene la logica para las pruebas del ejercicio de FizzBuzz
+"""
---
+++ tdd_exercise/test_search_functionality.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""
+Este modulo contiene las pruebas del ejercicio de search functionality para buscar ciudades
+"""
