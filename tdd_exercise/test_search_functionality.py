48211f28f67a260c262334731c48b149a3c2cba4
vela <dvela020@gmail.com>
2026-04-13 19:56:45 -0600

    feat: Exercise 4 Test Requirement 5

--- tdd_exercise/test_search_functionality.py
+++ tdd_exercise/test_search_functionality.py
@@ -6,7 +6,7 @@ Este modulo contiene las pruebas del ejercicio de search functionality para busc
 
 import unittest
 
-from search_functionality import search
+from search_functionality import read_from_json, search
 
 
 class TestSearchFunctionality(unittest.TestCase):
@@ -16,29 +16,34 @@ class TestSearchFunctionality(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        # Cargar ciudades del archivo json, read_from_json
+        cities = read_from_json("cities")
+
         cls.test_data = [
-            # Requerimiento 1: < 2 caracteres deben regresar no result
+            # 1. < 2 caracteres deben regresar no result
             {"input": "", "output": []},
             {"input": "a", "output": []},
-            # Requerimiento 2: >= 2 caracteres deberan regresar ciudades con busqueda de texto
+            # 2. >= 2 caracteres deberan regresar ciudades con busqueda de texto
             {"input": "Va", "output": ["Valencia", "Vancouver"]},
             {"input": "Pa", "output": ["Paris"]},
-            # Requerimiento 3: busqueda Case insensitive
+            # 3. Busqueda Case insensitive
             {"input": "va", "output": ["Valencia", "Vancouver"]},
             {"input": "PARIS", "output": ["Paris"]},
             {"input": "NeW", "output": ["New York City"]},
-            # Requerimiento 4: buscar texto como parte del nombre de la ciudad
+            # 4. Buscar texto como parte del nombre de la ciudad
             {"input": "ape", "output": ["Budapest"]},
             {"input": "ster", "output": ["Amsterdam"]},
             {"input": "kok", "output": ["Bangkok"]},
             {"input": "dam", "output": ["Rotterdam", "Amsterdam"]},
+            # 5. Devuelve todos los paises
+            {"input": "*", "output": cities},
             # Edge cases
             {"input": "xyz", "output": []},
         ]
 
     def test_search(self):
         """
-        Tests the search_cities function with various inputs and expected outputs.
+        Funcion que realiza ciclo para hacer pruebas con variedad de inputs
         """
         for x in self.test_data:
             with self.subTest(input=x["input"], output=x["output"]):
